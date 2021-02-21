fb9c1bd2feff962658b548d012955fa9646100a2
Kenua <jgvezon@gmail.com>
2021-02-20 21:28:02 -0500

    Notes can be saved in user's machine
    Now notes can be saved in localStorage or in a cookie. Also method clear added and minor changes.

--- notes.js
+++ notes.js
@@ -1,23 +1,8 @@
 "use strict";
 
-/*
-   Notes
-      prop notes 
-         an object that holds other objects with two properties: title and body.
-
-      prop index 
-         an integer used to indexed the identifier of notes prop. 
-
-      prop add 
-         A methods that adds a new note inside notes prop.
-
-      prop remove 
-      Methods that removes a note in notes prop.
-*/
-
 let notes = (function() {
    let notesSaved = {};     // holds the user's notes 
-   let index = 1;           // used to index the identifier of a note inside notesSaved
+   let index = 0;           // used to index the identifier of a note inside notesSaved
    const getNotes = function() { // returns notesSaved
       return notesSaved;
    };
@@ -32,14 +17,19 @@ let notes = (function() {
 
       return this;
    };
-   const remove = function(index = null) { // removes a note inside notesSaved
-      if (!index || !Number(index)) return this;
-      delete notesSaved[index];
+   const remove = function(pos = null) { // removes a note inside notesSaved
+      pos = Number(pos);
+      if (isNaN(pos) || pos > index) return this;
+      delete notesSaved[pos];
       return this;
-   }
-   const saveNotes = function() {
+   };
+   const clear = function() { // clears notesSaved
+      notesSaved = {};
+      return this;
+   };
+   const saveNotes = function() { // save notes in user's machine
       // check if localStorage exists
-      if (!window.localStorage) {
+      if (window.localStorage) {
          // save notes in localStorage
          let data = {
             notes: notesSaved,
@@ -50,18 +40,40 @@ let notes = (function() {
       } else {
          // save notes inside a cookie
          let data = JSON.stringify(this.getNotes());
-         document.cookie = `notes=${data}`;
+         document.cookie = `notes=${data}:note:;`;
+         document.cookie = `index=${index}:index:;`;
       }
+
+      return this;
+   };
+   const getSavedNotes = function() { // get notes in user's machine
+      if (window.localStorage) {
+         let data = JSON.parse(localStorage.notes);
+
+         notesSaved = data.notes;
+         index = data.index;
+      } else {
+         let data = document.cookie;
+         let notes = data.substring( data.indexOf("notes="), data.indexOf(":note:") ).replace("notes=", "");
+         let index = data.substring( data.indexOf("index="), data.indexOf(":index:") ).replace("index=", "");
+
+         notesSaved = JSON.parse(notes);
+         index = index;
+      }
+
+      return this;
    };
 
    return {
       getNotes,
       add,
       remove,
+      clear,
       saveNotes,
+      getSavedNotes,
    };
 })();
 
 notes.add("note 1", "# note 1 ## About this note...")
-     .add("note 2", "# note 3 ## About this note...")
+     .add("note 2", "# note 2 ## About this note...")
      .add("note 3", "# note 3 ## About this note...");
